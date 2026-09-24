c962418852eac7f94246ca28e8627b37769f070b
elvisalexander <elvis_xel@hotmail.com>
2026-09-23 21:07:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import pandas as pd
 st.title('Bolsa de Valores Quito BI')
 st.sidebar.title('Parámetros')
 st.write('Elaborado por: Elvis Torres')
+
+archivo=st.sidebar.file_uploader('Cargue su archivo')
+
+if archivo is not None:
+    tabla = pd.read_csv(archivo)
+    st.write(tabla)
